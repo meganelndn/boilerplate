072ee7051bc7af9b3c398ebf448ce77bd508e12a
Mégane <megane.londono@hotmail.com>
2021-12-08 13:09:05 +0100

    boilerplate files for future use

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import { styleBody, addTitle, contact } from "./dom";
+import users, { getPremUsers } from "./data";
+
+const premUsers = getPremUsers(users);
+console.log(users, premUsers);
+
+console.log("test2");
+console.log("test3");
